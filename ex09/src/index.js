0ab1db5450ad642eaf185edd4ab75f58c2c65684
TA <TA@TAs-iMac.local>
2021-09-15 23:18:03 +0200

    JavaScript project ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    //Variables
+
+var difference = numOne - numTwo; 
+
+var numOne; 
+
+var numTwo; 
+
+    //Only change code above this line
+    return difference; 
+}
+
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
+
+module.exports = main;
