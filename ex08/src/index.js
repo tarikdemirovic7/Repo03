0c710d4ca91eef48d7dda082796ef8feb0fe5b85
TA <TA@TAs-iMac.local>
2021-09-15 23:12:13 +0200

    JavaScript project ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    //Variables
+
+var sum = numOne + numTwo; 
+
+var numOne; 
+
+var numTwo; 
+
+    //Only change code above this line
+    return sum; 
+}
+
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
+
+
