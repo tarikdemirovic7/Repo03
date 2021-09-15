4b0e9541323c50b4ca41377eb38096df1f37801e
TA <TA@TAs-iMac.local>
2021-09-15 22:04:50 +0200

    JavaScript project ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    //Only change code below this line
+
+// This is a single style JavaScript comment
+
+/* document write ("This is a multi-line comment!");
+document write ("The second row of multi-line comment");
+document write ("The third row of multi-line comment"); 
+document write ("The fourt row of multi-line comment");
+document write ("The fifth row of multi-line comment");
+*/
+    //Only change code above this line
+    
+}
+
+console.log(main());
+
+module.exports = main; 
