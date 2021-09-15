0969aad69d10a1f894b0c5a1ed5f8829cd6fc05e
TA <TA@TAs-iMac.local>
2021-09-15 22:45:42 +0200

    JavaScript project ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,25 @@
+function main() {
+    //Only change code below this line
+
+var x;
+x = 7; 
+
+var y; 
+y = 12; 
+
+var z; 
+z = "My name is ";
+
+    //Only change code above this line
+    x = x + 2; 
+
+    y = y + 4; 
+
+    z = z + "John!"; 
+
+    return [x, y, z];
+}
+
+console.log(main());
+
+module.exports = main;
