97b68d83bec08ea7a171994ec42bcea7562fa5d0
TA <TA@TAs-iMac.local>
2021-09-15 22:31:50 +0200

    JavaScript project ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+
+var x;
+x = 10; 
+var y; 
+y = x;
+
+    //Only change code above this line
+    return [x, y];
+}
+
+console.log(main());
+
+module.exports = main;
