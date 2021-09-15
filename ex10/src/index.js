2011b095176e380f549504564cb620ddc8738946
TA <TA@TAs-iMac.local>
2021-09-15 23:22:43 +0200

    JavaScript project ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    //Variables
+
+var product = numOne * numTwo; 
+
+var numOne; 
+
+var numTwo; 
+
+    //Only change code above this line
+    return product; 
+}
+
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+module.exports = main;
